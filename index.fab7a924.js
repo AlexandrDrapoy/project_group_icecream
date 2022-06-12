7f38b60b1f39821b30d07d9f267e40815ec28a3c
AlexandrDrapoy <dr4alex@gmail.com>
2022-06-12 08:21:32 +0000

    Deploying to gh-pages from @ AlexandrDrapoy/project_group_icecream@22521ed1ed7674b631ebbf4f648efb9b5ca9762c 🚀

---
+++ index.fab7a924.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function e(){document.body.classList.toggle("modal-open"),o.modal.classList.toggle("backdrop--is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.fab7a924.js.map
