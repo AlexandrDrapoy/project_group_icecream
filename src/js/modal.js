d21cf4fd7cbddc6d905d3c42c637fd39068f42cb
Alexander Drapoy <dr4alex@gmail.com>
2022-06-13 21:48:29 +0300

    update modal.js and mobile-menu.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,9 +1,12 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
+    openModalBtn1: document.querySelector('[data-modal-open-mob]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
   };
+
+  refs.openModalBtn1.addEventListener('click', toggleModal_1);
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
@@ -12,4 +15,9 @@
     document.body.classList.toggle('modal-open');
     refs.modal.classList.toggle('backdrop--is-hidden');
   }
+  function toggleModal_1() {
+    document.body.classList.toggle('modal-open-mob');
+    refs.modal.classList.toggle('backdrop--is-hidden');
+  }
 })();
+('onclick');
